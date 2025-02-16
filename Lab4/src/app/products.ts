f0c71d9ffaabae259eb33f4b262cf509a8e5f951
ARDEVIE <almasbekov.damir777@gmail.com>
2025-02-16 22:27:38 +0500

    .

---
+++ Lab4/src/app/products.ts
@@ -0,0 +1,113 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  images: object;
+  prodLink: string;
+  rating: number;
+  slideNumber: number;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple iPhone 16 Pro Max 256Gb',
+    price: 1199,
+    description: 'A 6.9-inch premium smartphone with advanced AI, a larger display, and superior cameras',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/hcf/h69/87295489343518.png?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-16-pro-max-256gb-chernyi-123787551/?c=750000000",
+    rating: 4.8,
+    slideNumber: 1,
+  },
+  {
+    id: 2,
+    name: 'Apple iPhone 16 Pro 256Gb',
+    price: 999,
+    description: 'A 6.3-inch Pro device with new camera technology and a faster A18 chip',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/h7b/hff/87295485018142.png?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-16-pro-256gb-belyi-123888651/",
+    rating: 4.5,
+    slideNumber: 1,
+  },
+  {
+    id: 3,
+    name: 'Apple iPhone 16 256Gb',
+    price: 799,
+    description: 'A 6.1-inch device with improved AI features and enhanced battery efficiency',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/hf3/h65/87295470731294.png?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-16-128gb-chernyi-123713453/?c=750000000",
+    rating: 4.5,
+    slideNumber: 1,
+  },
+  {
+    id: 4,
+    name: 'Apple iPhone 15 Pro Max 256Gb',
+    price: 1099,
+    description: 'A 6.7-inch powerhouse with a periscope zoom lens and titanium build',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/ha3/hda/86319874048030.jpg?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-15-pro-max-256gb-seryi-113138420/?c=750000000",
+    rating: 4.5,
+    slideNumber: 1,
+  },
+  {
+    id: 5,
+    name: 'Apple iPhone 15 Pro 256Gb',
+    price: 999,
+    description: 'A 6.1-inch titanium frame phone with the A17 Pro chip and advanced cameras',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/hc6/h71/86302551736350.jpg?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-15-pro-256gb-sinii-113138331/?c=750000000",
+    rating: 4.3,
+    slideNumber: 1,
+  },
+  {
+    id: 6,
+    name: 'Apple iPhone 15 256Gb',
+    price: 799,
+    description: 'A 6.1-inch smartphone with USB-C and an enhanced camera system',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/hab/h93/86303746392094.jpg?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-15-256gb-goluboi-113137931/",
+    rating: 4.2,
+    slideNumber: 1,
+  },
+  {
+    id: 7,
+    name: 'Apple iPhone 14 Pro Max 256Gb',
+    price: 1099,
+    description: 'A 6.7-inch flagship with advanced cameras and the A16 Bionic chip',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/hd6/h0f/64428189384734.jpg?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-zolotistyi-106363335/",
+    rating: 4.6,
+    slideNumber: 1,
+  },
+  {
+    id: 8,
+    name: 'Apple iPhone 14 Pro 256Gb',
+    price: 999,
+    description: 'A 6.1-inch device with Dynamic Island and a 48MP main camera',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/h7b/hdc/64428132991006.jpg?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-14-pro-256gb-fioletovyi-106363319/",
+    rating: 4.5,
+    slideNumber: 1,
+  },
+  {
+    id: 9,
+    name: ' Apple iPhone 14 256Gb',
+    price: 799,
+    description: 'A 6.1-inch smartphone with improved low-light photography and safety features',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/hf7/h35/86042946469918.png?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-14-256gb-krasnyi-106363188/",
+    rating: 4.5,
+    slideNumber: 1,
+  },
+  {
+    id: 10,
+    name: 'Apple iPhone 13 Pro Max 256Gb',
+    price: 1099,
+    description: 'A large 6.7-inch screen with exceptional battery life and pro-level cameras',
+    images: ['https://resources.cdn-kaspi.kz/img/m/p/hf4/h2b/86009680822302.png?format=gallery-medium'],
+    prodLink: "https://kaspi.kz/shop/p/apple-iphone-13-pro-max-256gb-zelenyi-104079153/?c=750000000",
+    rating: 4.4,
+    slideNumber: 1,
+  },
+];
