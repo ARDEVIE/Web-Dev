8fa8554e408dc1b1a335f670986303ac6160564d
ARDEVIE <almasbekov.damir777@gmail.com>
2025-03-02 17:22:11 +0500

    .

---
+++ Lab6/CRUD-example/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators'; 
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project';
+  
+}
